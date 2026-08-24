86b5ffb8dec682f7d61b351b1ff13356a5ee93fc
bhavy1204 <bhavysarwa51@gmail.com>
2026-08-24 18:26:46 +0530

    chore: ts config

--- packages/search-engine/src/tokenizer.test.ts
+++ packages/search-engine/src/tokenizer.test.ts
@@ -2,3 +2,5 @@ import { tokenize } from "./tokenizer";
 
 console.log(tokenize("function debounce(fn, wait)"));
 console.log(tokenize("getUserById"));
+console.log(tokenize("fetch_user_data"));
+console.log(tokenize("MAX_RETRY_COUNT"));
